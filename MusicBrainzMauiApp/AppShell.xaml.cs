e043bd2fc46bc9dd96d5fcf1474227b9194bd2be
CmdrHitch <andy.cope.99@gmail.com>
2022-05-09 18:20:02 +0100

    first commit

---
+++ MusicBrainzMauiApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicBrainzMauiApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MusicBrainzMauiApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MusicBrainzMauiApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
