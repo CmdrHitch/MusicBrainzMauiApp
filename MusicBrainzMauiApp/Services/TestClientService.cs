37523c54e4518e3c403711d65c48231dea2ffbcd
CmdrHitch <andy.cope.99@gmail.com>
2022-05-17 16:59:14 +0100

    adding test

---
+++ MusicBrainzMauiApp/Services/TestClientService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicBrainzMauiApp.Services
+{
+  public static class TestClientService
+    {
+        public async Task<Dictionary>GetDictionaryAsync(string SearchExpr)
+        {
+           await Task.Run() 
+        }
+        
+    }
+}
